fbb255b2a71f269aa47b81c1fd26c8b0fa08035b
InkaDrum <katharinagirndt@yahoo.de>
2021-08-11 10:42:24 +0200

    new build

--- src/components/End.js
+++ src/components/End.js
@@ -17,20 +17,17 @@ const End = (props) => {
             {/* <p>Klicken Sie jetzt bitte auf den Button, um zur Umfrage zu gelangen</p> */}
             <p>
               {" "}
-              Klicken Sie jetzt bitte auf den Button, um zur zUmfrage zu
-              gelangen
+              Klicken Sie jetzt bitte auf den Button, um zur Umfrage zu gelangen
               <br></br>
               <br></br>
             </p>
           </div>
         </header>
         <div id="containerButtonEnd">
-          <a href="https://www.survio.com/survey/d/C3K5I4C4W7L5R1J7O">
+          {/* <a href="https://www.survio.com/survey/d/C3K5I4C4W7L5R1J7O">
             Umfrage starten
-          </a>
-          {/* <a href="https://kaiuby2.github.io">
-            App-Version 2 starten
           </a> */}
+          <a href="https://kaiuby2.github.io">App-Version 2 starten</a>
         </div>
       </form>
     </div>
